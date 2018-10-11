43d74776cbc30971d1dd75c69d1aa5e2bb42197a
nathanyee <nathaniel.yee@students.olin.edu>
2018-10-11 19:46:10 -0400

    did decoders

--- decoders.v
+++ decoders.v
@@ -12,3 +12,15 @@ input[4:0]	address
 
 endmodule
 
+/*
+/ << is the bitshift operator.
+/ 
+/ The enable bit (either 1 or 0) is bitshifted address number of times. For example,
+/ adress=00011, enable=1 would result in 3 bit shifts:
+/ 
+/ out=000000000000000000000000000001  //starting bit
+/ out=000000000000000000000000000010  //shift 1
+/ out=000000000000000000000000000100  //shift 2
+/ out=000000000000000000000000001000  //shift 3
+/ 
+*/
