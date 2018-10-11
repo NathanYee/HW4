0b03c86a963c1f4063ea157292551297578492ea
nathanyee <nathaniel.yee@students.olin.edu>
2018-10-11 17:56:22 -0400

    did deliverables 2,3 for HW4

--- register.v
+++ register.v
@@ -15,3 +15,37 @@ input		clk
     end
 
 endmodule
+
+// 32-bit register with enable
+module register32
+(
+output reg[31:0] q,
+input[31:0]      d,
+input            wrenable,
+input            clk
+);
+
+    always @(posedge clk) begin
+        if(wrenable) begin
+            q = d;
+        end
+    end
+
+endmodule
+
+// 32-bit register that only outputs zero
+module register32zero
+(
+output reg[31:0] q,
+input[31:0]      d,
+input            wrenable,
+input            clk
+);
+
+    always @(posedge clk) begin
+        if(wrenable) begin
+            q = 0;
+        end
+    end
+    
+endmodule
